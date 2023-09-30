ade4868e1b83c0155501105d2a6032ee50da2bcd
Yaroslav <otebator@gmail.com>
2023-09-30 23:43:42 +1000

    Initial Commit

---
+++ lab_1.1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  lab_1.1
+//
+//  Created by Ярослав on 30.09.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
